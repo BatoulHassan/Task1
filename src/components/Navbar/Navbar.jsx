47cff2d90b71f22674d1feac878bd30c312faa73
Batoul Hassan <batoulhassan264@gmail.com>
2023-08-18 15:00:31 +0300

    modify on navbar background

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,32 @@
+import { useState } from 'react';
 import Logo from '../Logo/Logo';
 import  './NavbarStyle.css';
 
 
 const Navbar = ({gitBtn, signBtn}) => {
+
+  // change nav color when scrolling
+  const [color, setColor] = useState(false);
+
+  const changeColor = () => {
+    if(window.scrollY > 60) {
+      setColor(true)
+    } else {
+      setColor(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeColor)
+
   return (
-    <nav>
+      <nav className={color ? 'navbar navbar-bg' : 'navbar'}>
         <div className="navContainer">
-          <Logo />
-          <div className='btnContainer'>
-            <button>{gitBtn}</button>
-            <button>{signBtn}</button>
-          </div>
-        </div>
+             <Logo />
+             <div className='btnContainer'>
+               <button>{gitBtn}</button>
+               <button>{signBtn}</button>
+             </div>
+           </div>
     </nav>
   )
 }
